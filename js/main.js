7d95e8bb1e011d574b754fb16a87cd8a4c4a750b
Desenvolvimento15 <96912707+Desenvolvimento15@users.noreply.github.com>
2024-01-19 11:22:55 -0300

    vs01

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$('.galeria').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  lazyLoad: 'ondemand', controls: false,
+   
+});
+
+$('.galeria-5').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  dots: false,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  lazyLoad: 'ondemand', controls: false,
+   
+});
+		
+
+
+$('.multiple-items').slick({
+  arrows: false,
+  infinite:true,
+  focusOnSelect: true,
+  variableWidth: true,
+  autoplay: true,
+  autoplaySpeed: 1000
+ 
+});
